9c294a60ba7e4ba6cb911a21495d592fe27e9be1
Jan Youssef <janyoussef19@gmail.com>
2023-05-02 18:08:41 +0300

    create concrete impls for chair

---
+++ src/main/java/com/jan/abstractfactory/chairs/ClownChair.java
@@ -0,0 +1,28 @@
+package com.jan.abstractfactory.chairs;
+
+import com.jan.abstractfactory.interfaces.Chair;
+
+public class ClownChair implements Chair {
+    private final String name;
+    private final String color;
+
+    public ClownChair(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public void sitOn() {
+        System.out.println("Sitting on" + this.name + " a " + this.getColor() + " " + this.getClass()
+                .getSimpleName() + " chair.");
+
+    }
+}
