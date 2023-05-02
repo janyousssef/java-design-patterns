b4ffd3521eae99442f72a551f3c22ed23f659f12
Jan Youssef <janyoussef19@gmail.com>
2023-05-03 02:58:35 +0300

    implement commands

--- src/main/java/com/jan/command/bankcommands/DepositMoney.java
+++ src/main/java/com/jan/command/bankcommands/DepositMoney.java
@@ -1,13 +1,23 @@
 package com.jan.command.bankcommands;
 
+import com.jan.command.domain.BankUser;
+
 public class DepositMoney implements BankCommand {
+    private final BankUser user;
+    private final long amount;
+
+    public DepositMoney(BankUser user, long amount) {
+        this.user = user;
+        this.amount = amount;
+    }
+
     @Override
     public void execute() {
-
+        user.deposit(amount);
     }
 
     @Override
     public void undo() {
-
+        user.withdraw(amount);
     }
 }
--- src/main/java/com/jan/command/bankcommands/TransferMoney.java
+++ src/main/java/com/jan/command/bankcommands/TransferMoney.java
@@ -1,13 +1,27 @@
 package com.jan.command.bankcommands;
 
+import com.jan.command.domain.BankUser;
+
 public class TransferMoney implements BankCommand {
+    private final BankUser sourceUser;
+    private final BankUser destinationUser;
+    private final long amount;
+
+    public TransferMoney(BankUser sourceUser, BankUser destinationUser, long amount) {
+        this.sourceUser = sourceUser;
+        this.destinationUser = destinationUser;
+        this.amount = amount;
+    }
+
     @Override
     public void execute() {
-
+        sourceUser.withdraw(amount);
+        destinationUser.deposit(amount);
     }
 
     @Override
     public void undo() {
-
+        destinationUser.withdraw(amount);
+        sourceUser.deposit(amount);
     }
 }
