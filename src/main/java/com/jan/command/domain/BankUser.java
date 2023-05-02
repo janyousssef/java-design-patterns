2742c3a9f3d1ab4a27af5a784c4cc7591e7d42db
Jan Youssef <janyoussef19@gmail.com>
2023-05-03 02:53:52 +0300

    add user class

---
+++ src/main/java/com/jan/command/domain/BankUser.java
@@ -0,0 +1,23 @@
+package com.jan.command.domain;
+
+public class BankUser {
+    private String name;
+    private long balance;
+
+    public BankUser(String name, long balance) {
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public void deposit(long amount) {
+        balance += amount;
+    }
+
+    public void withdraw(long amount) {
+        balance -= amount;
+    }
+
+    public void printBalance() {
+        System.out.println("Balance of " + name + " is " + balance);
+    }
+}
