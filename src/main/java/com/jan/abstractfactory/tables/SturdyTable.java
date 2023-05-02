b0fb3f9fa34e2e23d70b360f0c536c16d8c05ca2
Jan Youssef <janyoussef19@gmail.com>
2023-05-02 18:16:44 +0300

    create concrete impls for table

---
+++ src/main/java/com/jan/abstractfactory/tables/SturdyTable.java
@@ -0,0 +1,27 @@
+package com.jan.abstractfactory.tables;
+
+import com.jan.abstractfactory.interfaces.Table;
+
+public class SturdyTable implements Table {
+    private final String name;
+    private final String color;
+
+    public SturdyTable(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public void sitAt() {
+        System.out.println("Sitting at " + this.name + " a " + this.getColor() + " " + this.getClass()
+                .getSimpleName() + " table.");
+    }
+}
---
+++ src/main/java/com/jan/abstractfactory/tables/WoodenTable.java
@@ -0,0 +1,27 @@
+package com.jan.abstractfactory.tables;
+
+import com.jan.abstractfactory.interfaces.Table;
+
+public class WoodenTable implements Table {
+    private final String name;
+    private final String color;
+
+    public WoodenTable(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public void sitAt() {
+        System.out.println("Sitting at " + this.name + " a " + this.getColor() + " " + this.getClass()
+                .getSimpleName() + " table.");
+    }
+}
