49fddf94aabd968da5c0041ff2629e592d9cb67a
Jan Youssef <janyoussef19@gmail.com>
2023-05-03 03:24:25 +0300

    refactor runner

--- src/main/java/com/jan/command/CommandRunner.java
+++ src/main/java/com/jan/command/CommandRunner.java
@@ -6,32 +6,16 @@ import com.jan.command.domain.BankCommandInvoker;
 import com.jan.command.domain.BankUser;
 
 public class CommandRunner {
+    static final BankUser jan = new BankUser("Jan", 1000);
+    static final BankUser mostafa = new BankUser("Mostafa", 0);
+    static final BankCommandInvoker invoker = new BankCommandInvoker();
+
     public static void main(String[] args) {
-        BankUser jan = new BankUser("Jan", 1000);
-        BankUser mostafa = new BankUser("Mostafa", 0);
-        System.out.println("---------Balances before transfer---------");
-        jan.printBalance();
-        mostafa.printBalance();
+        part1();
+        part2();
+    }
 
-        System.out.println("---------Transfer 500 from Jan to Mostafa---------");
-        TransferMoney transfer500 = new TransferMoney(jan, mostafa, 500);
-        BankCommandInvoker invoker = new BankCommandInvoker(transfer500);
-        invoker.execute();
-
-        System.out.println("---------Balances after transfer---------");
-        jan.printBalance();
-        mostafa.printBalance();
-
-        System.out.println("---------Undo transfer---------");
-        invoker.undo();
-
-        System.out.println("---------Balances after undo---------");
-        jan.printBalance();
-        mostafa.printBalance();
-
-        System.out.println("---------Transfers log---------");
-        invoker.printLog();
-
+    private static void part2() {
         System.out.println("---------Change command---------");
         DepositMoney deposit100 = new DepositMoney(jan, 100);
         invoker.setCommand(deposit100);
@@ -43,6 +27,31 @@ public class CommandRunner {
         invoker.execute();
 
         System.out.println("---------Balances after deposit---------");
+        jan.printBalance();
+        mostafa.printBalance();
+
+        System.out.println("---------Transfers log---------");
+        invoker.printLog();
+    }
+
+    private static void part1() {
+        System.out.println("---------Balances before transfer---------");
+        jan.printBalance();
+        mostafa.printBalance();
+
+        System.out.println("---------Transfer 500 from Jan to Mostafa---------");
+        TransferMoney transfer500 = new TransferMoney(jan, mostafa, 500);
+        invoker.setCommand(transfer500);
+        invoker.execute();
+
+        System.out.println("---------Balances after transfer---------");
+        jan.printBalance();
+        mostafa.printBalance();
+
+        System.out.println("---------Undo transfer---------");
+        invoker.undo();
+
+        System.out.println("---------Balances after undo---------");
         jan.printBalance();
         mostafa.printBalance();
 
